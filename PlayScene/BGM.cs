a03548d679b376438acde6261f9e4deec5ac8cb3
Take-Kai <tooyaaa_030303@icloud.com>
2024-05-19 15:01:36 +0900

    Update BGM.cs

--- PlayScene/BGM.cs
+++ PlayScene/BGM.cs
@@ -1,1 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class BGM : MonoBehaviour
+{
+    public GameObject start;
+    AudioSource audioData;
+    public AudioClip Minzoku33;
+
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.PlayOneShot(Minzoku33);
+    }
+}
