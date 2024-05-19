f51cb8c0c5767ca58e2135a2631b1f3428b435ec
Take-Kai <tooyaaa_030303@icloud.com>
2024-05-19 15:04:37 +0900

    Update BackTitle.cs

--- ResultScene/BackTitle.cs
+++ ResultScene/BackTitle.cs
@@ -1,1 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
+public class BackTitle : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
